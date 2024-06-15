44a290fba94e9fee2a346dcdde26550ee4b860bc
Ravikachhadiya <ravikachhadiya37@gmail.com>
2024-06-15 18:43:05 +0530

    mongoDB connection

--- index.js
+++ index.js
@@ -1,117 +1,147 @@
 const express = require("express");
 const fs = require("fs");
-var users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
+
+// getting mock data from mock json file
+// var users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
+
+// Connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/nodejs-learning-1")
+  .then(() => console.log("MongoDB connected!"))
+  .catch((error) => console.log("MongoDB Error", error));
+
+// Schema
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    job_title: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("user", userSchema);
+
 const USERS_FILE_PATH = "./MOCK_DATA.json";
 
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-	console.log("Middleware 2 is called");
-	fs.appendFile(
-		"./log.txt",
-		`\n${Date.now()}:${req.ip} ${req.method}: ${req.path}`,
-		(err, data) => {
-			next();
-		}
-	);
+  console.log("Middleware 2 is called");
+  fs.appendFile(
+    "./log.txt",
+    `\n${Date.now()}:${req.ip} ${req.method}: ${req.path}`,
+    (err, data) => {
+      next();
+    }
+  );
 });
 
 app.use((req, res, next) => {
-	console.log("Middleware 3 is called");
+  console.log("Middleware 3 is called");
 
-	next();
+  // setting custom header.
+  // Always put X- as a prefix to follow best practice.
+  res.setHeader("X-My-Name", "ravi kachhadiya");
+
+  next();
 });
 
 // Routes
-app.get("/users", (req, res) => {
-	const html = `
+app.get("/users", async (req, res) => {
+  const allDBUsers = await User.find({});
+  const html = `
     <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    ${allDBUsers
+      .map((user) => `<li>${user.first_name} - ${user.email}</li>`)
+      .join("")}
     </ul>
     `;
 
-	return res.send(html);
+  return res.send(html);
 });
 
-app.get("/api/users", (req, res) => {
-	// setting custom header.
-	// Always put X- as a prefix to follow best practice.
-	res.setHeader("X-My-Name", "ravi kachhadiya");
-	return res.json(users);
+app.get("/api/users", async (req, res) => {
+  const allDBUsers = await User.find({});
+
+  return res.json(allDBUsers);
 });
 
-app.post("/api/users", (req, res) => {
-	const body = req.body;
+app.post("/api/users", async (req, res) => {
+  const body = req.body;
 
-	if (
-		!body ||
-		!body.first_name ||
-		!body.last_name ||
-		!body.email ||
-		!body.gender ||
-		!body.job_title
-	) {
-		return res.status(400).json({ message: "All fields are required!" });
-	}
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
 
-	const id = users.length + 1;
-	users.push({ id, ...body });
+  const result = await User.create({
+    first_name: body.first_name,
+    last_name: body.last_name,
+    gender: body.gender,
+    email: body.email,
+    job_title: body.job_title,
+  });
+  console.log("result", result);
 
-	// updating file
-	fs.writeFile(USERS_FILE_PATH, JSON.stringify(users), (err, data) => {
-		return res.status(201).json({ status: "success", id });
-	});
+  return res.status(200).json({ message: "success" });
 });
 
 app
-	.route("/api/users/:id")
-	.get((req, res) => {
-		const id = Number(req.params.id);
-		const user = users.find((user) => user.id === id);
-		if (!user) {
-			return res.status(404).json({ message: "record not found!" })
-		}
+  .route("/api/users/:id")
+  .get(async (req, res) => {
+    const user = await User.findById(req.params.id);
 
-		res.json(user);
-	})
-	.patch((req, res) => {
-		const id = Number(req.params.id);
-		const body = req.body;
+    if (!user) {
+      return res.status(404).json({ message: "record not found!" });
+    }
 
-		if (!users.find((user) => user.id === id))
-			return res.status(404).json({ status: "success", id, message: "invalid id" });
+    res.json({ status: "success", user });
+  })
+  .patch(async (req, res) => {
+    await User.findByIdAndUpdate(req.params.id, req.body);
 
-		users = users.map((user) => (user.id === id ? { ...user, ...body } : user));
-
-		// updating file
-		fs.writeFile(USERS_FILE_PATH, JSON.stringify(users), (err, data) => {
-			return res.json({
-				status: "success",
-				id,
-				message: "successfully updated",
-			});
-		});
-	})
-	.delete((req, res) => {
-		const id = Number(req.params.id);
-
-		if (!users.find((user) => user.id === id))
-			return res.status(404).json({ status: "success", id, message: "invalid id" });
-
-		users = users.filter((user) => user.id !== id);
-
-		// updating file
-		fs.writeFile(USERS_FILE_PATH, JSON.stringify(users), (err, data) => {
-			return res.json({
-				status: "success",
-				id,
-				message: "successfully deleted",
-			});
-		});
-	});
+    return res.json({
+      status: "success",
+      id: req.params.id,
+      message: "successfully updated",
+    });
+  })
+  .delete(async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({
+      status: "success",
+      id: req.params.id,
+      message: "successfully deleted",
+    });
+  });
 
 app.listen(PORT, () => console.log("Server started!"));
